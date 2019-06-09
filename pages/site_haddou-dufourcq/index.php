37c001790606392622d1f50b0d2acb6eb9e47ea5
PaulHaddou <paul2405.63@hotmail.fr>
2019-06-09 19:45:53 +0200

    v1.5.1 - :art::building_construction: Addition of variables in the template of the project page, allowing to fill in the information quickly.

--- pages/site_haddou-dufourcq/index.php
+++ pages/site_haddou-dufourcq/index.php
@@ -29,27 +29,42 @@
     include '../../includes/navbarMobile.php';
     include '../../includes/sidebar.php';
   ?>
+  <!-- All the informations for the project -->
+  <?php
+    $projectImg = '../../img/site_haddou-dufourcq/site_haddou-dufourcq.jpg';
+    $projectImgAlt = 'Image représentant la home du site Haddou-Dufourcq.com';
+    $projectTitle = 'Site Haddou-Dufourcq';
+    $projectLinkHref = 'https://www.haddou-dufourcq.com';
+    $projectLink = 'www.haddou-dufourcq.com';
+    $projectYear = '2017 - 2018';
+    $projectRoles = 'Webmaster / Front-End';
+    $projectCategory = 'Dev / Design';
+    $projectTechnos = 'HTML - CSS - PHP';
+    $projectText1 = "Le projet consistait en la réalisation d'un site vitrine pour architectes d'intérieurs. Il s'agissait d'un devoir de développement pour le troisième trimestre de ma première année à HETIC. La v1 du site est sortie en juin 2018. Produite en 5h, elle était satisfaisante mais le code et la techno utilisés n'étaient pas vraiment au point avec les connaissances en a possession à ce moment là.";
+    $projectText2 = "J'ai donc décidé de reprendre le projet avec les clients en décembre dernier et retravaillé le site afin de l'améliorer et qu'il correspodde mieux à leurs attentes de professionnels: ajout de nouvelles pages, modification du l'UI, mise en place du HTTPS, … La v2 a donc pu être mise en ligne fin mai 2019. Avec du php, un .htacces et un robots.txt la techno a un peu évolué.Il me reste à revoir l'entièreté du code pour le rendre vraiment propre et ajouter notamment du scss.";
+    $projectText3 = "Durant ce projet j'ai été UI/UX en collaboration avec les clients, dev Front et webmaster. Ce projet a été très enrichissant car je m'en suis chargé de A à Z, je l'ai vu évoluer et il m'a permis de me mettre en situation réelle grâce aux échanges clients, que j'ai bien évidemment écoutés afin de répondre à leurs besoins mais que j'ai pu aussi conseiller, et réorienter lorsque cela me semblait nécessaire.";
+  ?>
   <!---------- CONTAINER --------->
     <span class="backSidebar"></span>
     <main>
-      <img src="../../img/site_haddou-dufourcq/site_haddou-dufourcq.jpg" alt="">
+      <img src="<?= $projectImg ?>" alt="<?= $projectImgAlt ?>">
       <div class="container">
         <div class="projectTitle">
-          <h3>Site Haddou-Dufourcq</h3>
-          <a href="https://www.haddou-dufourcq.com">www.haddou-dufourcq.com</a>
+          <h3><?= $projectTitle ?></h3>
+          <a href="<?= $projectLinkHref ?>"><?= $projectLink ?></a>
         </div>
         <div class="infosProject">
           <ul>
-            <li>2017 - 2018</li>
-            <li>Webmaster / Front-End</li>
-            <li>Dev / Design</li>
-            <li>HTML - CSS - PHP</li>
+            <li><?= $projectYear ?></li>
+            <li><?= $projectRoles ?></li>
+            <li><?= $projectCategory ?></li>
+            <li><?= $projectTechnos ?></li>
           </ul>
         </div>
         <div class="descriptionProject">
-          <p>Le projet consistait en la réalisation d'un site vitrine pour architectes d'intérieurs. Il s'agissait d'un devoir de développement pour le troisième trimestre de ma première année à HETIC. La v1 du site est sortie en juin 2018. Produite en 5h, elle était satisfaisante mais le code et la techno utilisés n'étaient pas vraiment au point avec les connaissances en a possession à ce moment là.</p>
-          <p>J'ai donc décidé de reprendre le projet avec les clients en décembre dernier et retravaillé le site afin de l'améliorer et qu'il correspodde mieux à leurs attentes de professionnels: ajout de nouvelles pages, modification du l'UI, mise en place du HTTPS, … La v2 a donc pu être mise en ligne fin mai 2019. Avec du php, un .htacces et un robots.txt la techno a un peu évolué.Il me reste à revoir l'entièreté du code pour le rendre vraiment propre et ajouter notamment du scss.</p>
-          <p>Durant ce projet j'ai été UI/UX en collaboration avec les clients, dev Front et webmaster. Ce projet a été très enrichissant car je m'en suis chargé de A à Z, je l'ai vu évoluer et il m'a permis de me mettre en situation réelle grâce aux échanges clients, que j'ai bien évidemment écoutés afin de répondre à leurs besoins mais que j'ai pu aussi conseiller, et réorienter lorsque cela me semblait nécessaire.</p>
+          <p><?= $projectText1 ?></p>
+          <p><?= $projectText2 ?></p>
+          <p><?= $projectText3 ?></p>
         </div>
       </div>
     </main>
